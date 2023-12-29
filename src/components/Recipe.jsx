f2f2676c46df30afca654f9a9e69a2b1b84ce7c6
asonkes <audrey.sonkes@gmail.com>
2023-12-29 21:37:12 +0100

    mise en place de coeur pour le like - augmentation de la base de données

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import styles from "./Recipe.module.scss";
 
 export default function Recipe({title, image}) {
+
+// Donc ici, on mettre en place "liked", et mettre que par défaut, il est faux, car par défault, le coeur est sans couleur...
+    const [liked, setLiked] = useState(false);
+
+// Et ici, au click, c'est l'inverse, oui puisqu'il change de couleur...
+    function handleClick() {
+        setLiked(!liked);
+    }
+
     return (
-        <div className={styles.recipe}>
+        <div onClick={handleClick} className={styles.recipe}>
             <div className={styles.imageContainer}>
                 <img src={image} alt="Recette du menu" />
             </div>
-            <div className={`p-10 d-flex align-items-center justify-content-center ${styles.recipeTitle}`}>
-                <h3>{ title }</h3>
+            <div className={`p-10 d-flex flex-column align-items-center justify-content-center ${styles.recipeTitle}`}>
+                <h3 className="mb-10">{ title }</h3>
+{/** Ici, on a fait une condition ternaire, si c'est liké, alors la couleur change, ausinon, c'est faux, donc la couleur part */}
+                <i className={`fa-solid fa-heart ${liked ? "text-primary" : ""}`}></i>
             </div>
         </div>
     );
--- src/data/recipes.jsx
+++ src/data/recipes.jsx
@@ -3,9 +3,9 @@
 
 export const data = [
     {
-        title: 'Gâteau au chocoloat',
-        _id: '658f18aee165e3b1e5d1cd3a',
-        image: 'https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?q=80&w=2003&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        title: "Bol de nouilles à l'oeuf",
+        _id: '658f277ba078c5b21ccbb31c',
+        image: 'https://images.unsplash.com/photo-1540162416395-16f7dfbb68d1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         title: 'Hamburger Maison',
@@ -33,9 +33,9 @@ export const data = [
         image: 'https://plus.unsplash.com/premium_photo-1664472607170-692bc1d09eb8?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
-        title: 'Salade Maison',
-        _id: '',
-        image: 'https://images.unsplash.com/photo-1540420773420-3366772f4999?q=80&w=1968&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        title: 'Assiette de viendes grillées',
+        _id: '658f271b8fe04990c1895a14',
+        image: 'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         title: 'Salade Gourmande avec poulet',
@@ -46,5 +46,50 @@ export const data = [
         title: 'Miel, Bananes et Myrtilles',
         _id: '658f196317b2f909830f2fcb',
         image: 'https://images.unsplash.com/photo-1484723091739-30a097e8f929?q=80&w=1547&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        title: 'Tarte aux framboises',
+        _id: '658f2769e506ed6d775dccfb',
+        image: 'https://images.unsplash.com/photo-1565958011703-44f9829ba187?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        title: 'Assiette de fruits',
+        _id: '658f275c79a21b3d890aade0',
+        image: 'https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGZvb2R8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+        title: 'Gâteau au chocoloat',
+        _id: '658f18aee165e3b1e5d1cd3a',
+        image: 'https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?q=80&w=2003&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        title: 'Soupe aux scampis et carottes',
+        _id: '658f2a6cff2df279366154f8',
+        image: 'https://plus.unsplash.com/premium_photo-1673809798703-6082a015f931?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGZvb2R8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+        title: 'Plateau de fromage',
+        _id: '658f2a5ffd7d550fbb8f8be0',
+        image: 'https://images.unsplash.com/photo-1517093602195-b40af9688b46?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDIyfHxmb29kfGVufDB8fDB8fHww'
+    },
+    {
+        title: 'Salade de chou-fleur avec grenades',
+        _id: '658f2a781e1f85b6dd99e550',
+        image: 'https://images.unsplash.com/photo-1510629954389-c1e0da47d414?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        title: 'Bol de ragout de saumon',
+        _id: '658f2a8bd5d86b05faf0aeaa',
+        image: 'https://images.unsplash.com/photo-1485921325833-c519f76c4927?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        title: 'Tacos cuits',
+        _id: '658f2a951956d42bcbb65dd3',
+        image: 'https://images.unsplash.com/photo-1565299585323-38d6b0865b47?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        title: 'Gauffres avec ses grains de café',
+        _id: '658f2be12e11adefccfba427',
+        image: 'https://images.unsplash.com/photo-1505253304499-671c55fb57fe?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
 ]
