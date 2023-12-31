302e8f0dd20077c65a3eeeebfbb0976e2009f43d
asonkes <audrey.sonkes@gmail.com>
2023-12-31 11:49:37 +0100

    bug avec classes - reintitialisation de mon projet - pas regarder si tt va bien avant...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,21 @@ import logo from "../assets/images/cookchef.webp";
 export default function Header() {
     return (
        <header className={ `d-flex flex-row align-items-center ${styles.header}`}>
-       <i className="fa-solid fa-bars"></i> 
        <div className="flex-fill">
        <img className="m-10" src={logo} alt="logo du restaurant" />
        </div>
        
-       <ul>
-        <button className="mr-15 btn btn-reverse-primary">
+       <div className={styles.headerList}>
+
+        <button className="mr-15 btn btn-reverse-primary" >
         <i className="fa-solid fa-basket-shopping mr-5"></i>
         Panier
         </button>
 
         <button className="btn btn-primary">Connexion</button>
-       </ul>
+       </div>
+
+       <i className={`fa-solid fa-bars ${styles.headerBurger}`}></i>
     </header>
     );
 }
