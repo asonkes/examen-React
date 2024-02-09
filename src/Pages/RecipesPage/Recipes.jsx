d71cdd60e73d3e4451db9c9061cdda9a5384e3b6
asonkes <audrey.sonkes@gmail.com>
2024-02-09 16:01:02 +0100

    final

--- src/Pages/RecipesPage/Recipes.jsx
+++ src/Pages/RecipesPage/Recipes.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getRecipeDetails } from "../../services/api";
 import recipes from './Recipes.module.scss';
+import { Link } from "react-router-dom";
 
 export default function Recipes() {
   const { id } = useParams(); // Récupère l'ID de l'URL
@@ -29,23 +30,22 @@ export default function Recipes() {
   if(isLoaded===true){
     return (
       <div className="flex-fill container">
-        <div className="card p-20 m-20">
-          <div className="d-flex flex-column align-items-center justify-content-center parentContainer2">
-            <div className="container2">
-            <h3 className="text-align-center m-20">{recipe.name}</h3>
-            <div className="d-flex flex-column align-items-center">
-               <img className={recipes.imageContainer} src={recipe.image} alt={recipe.title}></img>
-               </div>
-                  <h4 className="m-20 text-primary">Recipe</h4>
-                  <p className="m-20">Recette{recipe.instructions}</p>
-                  <h4 className="m-20 text-primary">Preparation Time</h4>
-                  <p className="m-20">{recipe.readyInMinutes}</p>
-               </div>
-            <div> 
-          </div>
-          </div>
-        </div>
+      <div className="card p-20 m-20">
+         <h3 className="text-align-center m-20">{recipe.name}</h3>
+         <div className="d-flex flex-column align-items-center">
+            <img className={recipes.imageContainer} src={recipe.image} alt={recipe.title}></img>
+            <h4 className="m-20 text-primary">Recipe</h4>
+            <p className="m-20">Recette{recipe.instructions}</p>
+            <h4 className="m-20 text-primary">Preparation Time</h4>
+            <p className="m-20">{recipe.readyInMinutes}</p>
+            <div className="d-flex justify-content-center">
+              <Link to="/">
+                <button className="btn-special btn-primary m-20">BACK</button>
+              </Link>
+            </div>
+         </div>
       </div>
+    </div>
     );
   }else{
     return (
